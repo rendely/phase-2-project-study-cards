8a4a6459b218d493b013e77cefd3d6f6eec3a67d
matthew rendely <matthew.rendely@gmail.com>
2023-04-22 21:34:19 -0700

    Install semantic UI. Test some components like cards, grid, progress bar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import 'semantic-ui-css/semantic.min.css'
+import { Card, Grid, Progress } from 'semantic-ui-react'
 import './App.css';
 
 
-function App() {
-  const [cards, setCards] = useState(undefined);
 
-  async function getCards(){
-    const response = await fetch('http://localhost:3001/cards');
-    const data = await response.json();
-    setCards(data);
+function App() {
+  const [cards, setCards] = useState([]);
+
+  function getCards() {
+    fetch('http://localhost:3001/cards')
+      .then(r => r.json())
+      .then(d => setCards(d))
   }
-  
+
+  useEffect(getCards, [])
+
   if (!cards) getCards();
 
   return (
     <div className="App">
       <header className="App-header">
-        Study Cards
+        Study Cards        
       </header>
       <main>
-        <div>
-       {cards ? cards.map(card => <div key={card.name}>{card.name}</div>) : 'no cards yet'}
-        </div>
+      <Progress total={cards.length} value={4} progress="ratio" indicating size="medium" />
+        <Grid doubling columns="4" padded> 
+          {cards ? cards.map(card =>
+          <Grid.Column key={card.name}>
+            <Card key={card.name} centered>
+              <Card.Content>
+                <Card.Header>
+                  {card.name}
+                </Card.Header>
+                <Card.Description>
+                  {card.body}
+                </Card.Description>
+              </Card.Content>
+            </Card>
+            </Grid.Column>
+            ) : 'no cards yet'}
+        </Grid>
       </main>
     </div>
   );
