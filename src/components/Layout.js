e5017c9f673da3a47488143bbda71590a25e8618
matthew rendely <matthew.rendely@gmail.com>
2023-04-23 18:17:17 -0700

    use Menu from semantic for navigation

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {useParams, NavLink} from 'react-router-dom'
+import {Menu} from 'semantic-ui-react'
+// import 'semantic-ui-css/semantic.min.css'
 
 function Layout({children}){
   
@@ -7,8 +9,10 @@ function Layout({children}){
 
   return (<div>
     <div>
-      <NavLink to="/" exact>Home</NavLink>
-      <NavLink to="/collections" exact>Collections</NavLink>
+      <Menu>
+        <Menu.Item as={NavLink} to="/" name="Home"></Menu.Item>
+        <Menu.Item as={NavLink} to="/collections" name="Collections"></Menu.Item>
+      </Menu>
     </div>
     Hello:
     {children}
