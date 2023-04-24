35c335ee921b8aaa0bf5f2f29332da55171babdf
matthew rendely <matthew.rendely@gmail.com>
2023-04-23 20:48:25 -0700

    Fix warnings

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
-import Layout from './Layout';
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 import Home from './Home';
