dc503610e7bcd84d93cf2887b25dd7a22d0749e4
matthew rendely <matthew.rendely@gmail.com>
2023-04-23 17:48:20 -0700

    Setup dummy routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import 'semantic-ui-css/semantic.min.css'
+import { Routes, Route } from "react-router-dom";
 import { Card, Grid, Progress } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
-
-
 
 function App() {
   const [cards, setCards] = useState([]);
@@ -16,34 +15,12 @@ function App() {
 
   useEffect(getCards, [])
 
-  if (!cards) getCards();
-
   return (
-    <div className="App">
-      <header className="App-header">
-        Study Cards        
-      </header>
-      <main>
-      <Progress total={cards.length} value={4} progress="ratio" indicating size="medium" />
-        <Grid doubling columns="4" padded> 
-          {cards ? cards.map(card =>
-          <Grid.Column key={card.name}>
-            <Card key={card.name} centered>
-              <Card.Content>
-                <Card.Header>
-                  {card.name}
-                </Card.Header>
-                <Card.Description>
-                  {card.body}
-                </Card.Description>
-              </Card.Content>
-            </Card>
-            </Grid.Column>
-            ) : 'no cards yet'}
-        </Grid>
-      </main>
-    </div>
-  );
+    <Routes>
+    <Route path="/" element={<div>Hello</div>} />
+    <Route path="/dog" element={<div>dog</div>} />
+  </Routes> 
+  )
 }
 
 export default App;
