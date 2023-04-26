adce488422bda401a4f84d683335708a1e3a71b0
matthew rendely <matthew.rendely@gmail.com>
2023-04-25 21:19:42 -0700

    Move CardForm to the Collection page and wire up adding a new card

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -2,27 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Button, Form, Dropdown, TextArea } from 'semantic-ui-react'
 
-import Layout from './Layout'
+function CardForm({onAddCard}) {
 
-function CardForm() {
-  const params = useParams();
-  const [collectionName, setCollectionName] = useState('');
-  const [formData, setFormData] = useState({
+  const formDataEmpty = {
     question: '',
-    answer: '',
-    collectionId: 0
-  })
+    answer: ''
+  };
 
-  useEffect(getCollectionInfo, [params]);
+  const [formData, setFormData] = useState(formDataEmpty)
 
-  function getCollectionInfo() {
-    fetch('http://localhost:3001/collections/' + params.id)
-      .then(r => r.json())
-      .then(collection => {
-        setCollectionName(collection.name);
-        setFormData({...formData, collectionId: collection.id});
-      })
-  }
 
   function handleChange(e, d){
     let key, val;
@@ -39,55 +27,24 @@ function CardForm() {
   
   function handleSubmit(e){
     e.preventDefault();
-    fetch('http://localhost:3001/cards', {
-      method: "POST",
-      headers: {"Content-type": "application/json"},
-      body: JSON.stringify(formData)
-    })
-    .then(r => r.json())
-    .then(newCard => console.log(newCard))
+    onAddCard(formData);
+    setFormData(formDataEmpty);
   }
 
-  // TODO: Swap this for fetched list of collections
-  const collectionOptions = [
-    {
-      key: 'Javascript',
-      text: 'Javascript',
-      value: 1   
-    },
-    {
-      key: 'html',
-      text: 'HTML',
-      value: 3  
-    }
-    ]
-
   return (
-    <Layout>
+    <div>
       <Form onSubmit={handleSubmit}>
         <Form.Field>
-        <label>Collection:</label>
-        <Dropdown
-        fluid
-        selection
-        name='collectionId'
-        id='collectionIdTest'
-        options={collectionOptions}
-        value={formData.collectionId}
-        onChange={handleChange}
-      />
-        </Form.Field>
-        <Form.Field>
         <label>Question:</label>
-          <TextArea name='question' placeholder='Add your question' onChange={handleChange}></TextArea>
+          <TextArea name='question' placeholder='Add your question' onChange={handleChange} value={formData.question}></TextArea>
         </Form.Field>
         <Form.Field>
         <label>Answer:</label>
-          <TextArea name='answer' placeholder='Add your answer' onChange={handleChange}></TextArea>
+          <TextArea name='answer' placeholder='Add your answer' onChange={handleChange} value={formData.answer}></TextArea>
         </Form.Field>
         <Button color={'green'} type='submit'>Add Card</Button>
       </Form>
-    </Layout>)
+    </div>)
 }
 
 export default CardForm
--- src/components/Collection.js
+++ src/components/Collection.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Button, Card, Container, Grid } from 'semantic-ui-react'
 import { useParams, Link } from 'react-router-dom'
 import Layout from './Layout'
+import CardForm from './CardForm'
 
 function Collection() {
   const params = useParams();
@@ -24,29 +25,46 @@ function Collection() {
       .then(collections => setCollectionName(collections[0].name))
   }
 
+  function handleAddCard(newCard) {
+    const formData = { ...newCard, collectionId: params.id };
+    fetch('http://localhost:3001/cards', {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(formData)
+    })
+      .then(r => r.json())
+      .then(newCard => setCards(cards => [...cards, newCard]))
+  }
+
   return (
     <Layout collectionName={collectionName}>
       <Container>
         <Grid columns={numColumns} >
           {cards.map(card =>
             <Grid.Column key={card.id}>
-               {/* TODO: break into own component  */}
-              <Card fluid >
-                <Card.Content textAlign='center' style={{ backgroundColor: "lightgray" }}>
+              {/* TODO: break into own component  */}
+              <Card fluid style={{ height: "100%" }}>
+                <Card.Content style={{ backgroundColor: "lightgray" }}>
                   {card.question}
                 </Card.Content>
-                <Card.Content textAlign='center'>
+                <Card.Content>
                   {card.answer}
                 </Card.Content>
               </Card>
             </Grid.Column>
           )}
+
         </Grid>
-      </Container>
-      <Container textAlign='center' style={{ padding: "40px" }}>
-        <Link to='./add'>
-        <Button color='green'>Add Card</Button>
-        </Link>
+        <Grid centered columns={2}>
+          <Grid.Column>
+            <Card fluid style={{ height: "100%" }}>
+              <Card.Content>
+                <CardForm onAddCard={handleAddCard} />
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        </Grid>
+
       </Container>
     </Layout>)
 }
