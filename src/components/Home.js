e4ca949027251b4c67e8ff69c6c33b2ad8dfcd8c
matthew rendely <matthew.rendely@gmail.com>
2023-04-27 00:22:52 -0700

    Add actions to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import Layout from './Layout'
 
 function Home() {
@@ -6,6 +7,7 @@ function Home() {
     <Layout>
       <h1>Study Cards</h1>
       <p>This is an app to let you create and review study cards.</p>
+      <p>To get started you can manage your <b><Link to='/collections'>collections</Link></b> or jump straight into <b><Link to='/review/'>reviewing</Link></b> cards.</p>
     </Layout>)
 }
 
