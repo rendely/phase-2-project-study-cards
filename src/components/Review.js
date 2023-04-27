de39bcea745492798dd231ddfae6d05541095e69
matthew rendely <matthew.rendely@gmail.com>
2023-04-27 00:16:53 -0700

    Add explanation for what keyboard shortcuts are

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from './Layout'
 import ReviewCard from './ReviewCard';
-import { Card, Container, Grid, Progress } from 'semantic-ui-react'
+import { Card, Container, Grid, Progress, Segment } from 'semantic-ui-react'
 
 function Review() {
 
@@ -10,9 +10,9 @@ function Review() {
   const [numCompleted, setNumCompleted] = useState(0);
 
   const currentCard = cards[0];
-  useEffect(loadCards,[])
+  useEffect(loadCards, [])
 
-  function loadCards(){
+  function loadCards() {
     fetch('http://localhost:3001/cards')
       .then(r => r.json())
       .then(cards => {
@@ -22,7 +22,7 @@ function Review() {
       })
   }
 
-  function updateCard(didGetIt){
+  function updateCard(didGetIt) {
     const formData = { needsReview: !didGetIt };
     const id = currentCard.id;
     console.log('updating!', formData, currentCard, id);
@@ -31,30 +31,34 @@ function Review() {
       headers: { "Content-type": "application/json" },
       body: JSON.stringify(formData)
     })
-    .then(r => r.json())
-    .then(updatedCard => {
-      setCards(cards => cards.filter(card => card.id !== updatedCard.id));
-      setNumCompleted(num => num+1);
-    })
+      .then(r => r.json())
+      .then(updatedCard => {
+        setCards(cards => cards.filter(card => card.id !== updatedCard.id));
+        setNumCompleted(num => num + 1);
+      })
   }
 
   return (
     <Layout>
       <Container text textAlign='center'>
-        <Progress total={totCards} value={numCompleted} progress='ratio'/>
+        <Progress total={totCards} value={numCompleted} progress='ratio' />
         <Grid columns={1} centered>
           <Grid.Column>
-            {currentCard ? 
-            <ReviewCard card={currentCard} updateCard={updateCard} />
-            :
-            <Card fluid style={{ height: "100%" }}>
-              <Card.Content>
-                Done! Congrats on completing your review!
+            {currentCard ?
+              <ReviewCard card={currentCard} updateCard={updateCard} />
+              :
+              <Card fluid style={{ height: "100%" }}>
+                <Card.Content>
+                  Done! Congrats on completing your review!
                 </Card.Content>
-            </Card>
+              </Card>
             }
           </Grid.Column>
         </Grid>
+        <Segment secondary>
+          <h3>Keyboard Shortcuts:</h3>
+          Down arrow: Show answer | Left arrow: Didn't know it | Right arrow: Got it!
+        </Segment>
       </Container>
     </Layout>)
 }
