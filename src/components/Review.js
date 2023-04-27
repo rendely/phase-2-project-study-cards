6b139aa8a8a4aa4564a61dc78fe231d5cf136d50
matthew rendely <matthew.rendely@gmail.com>
2023-04-26 22:43:04 -0700

    Review card interactions working, no http requests wired up yet

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Layout from './Layout'
 import ReviewCard from './ReviewCard';
-import { Container, Grid } from 'semantic-ui-react'
-
-
+import { Card, Container, Grid } from 'semantic-ui-react'
 
 function Review() {
+
+  const [cards, setCards] = useState([{question: 'hi', answer: 'yo', id:14}, {question: 'bye', answer: 'cya', id:15}]);
+  const [currentCard, setCurrentCard] = useState({});
+
+  useEffect(() => setCurrentCard(cards[0]),[cards]);
+
+  function loadCards(){
+    console.log('loaded');
+  }
+
+  function updateCard(){
+    console.log('updated');
+    setCards(cards.slice(1));
+  }
+
   return (
     <Layout>
       <Container>
         <Grid columns={3} centered>
           <Grid.Column>
-            <ReviewCard />
+            {currentCard ? 
+            <ReviewCard card={currentCard} updateCard={updateCard} />
+            :
+            <Card fluid style={{ height: "100%" }}>
+              <Card.Content>
+                Done! Congrats on completing your review!
+                </Card.Content>
+            </Card>
+            }
           </Grid.Column>
         </Grid>
       </Container>
--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,29 +1,33 @@
 import React, { useState } from 'react'
 import { Button, Card } from 'semantic-ui-react'
 
-function ReviewCard() {
+function ReviewCard({card, updateCard}) {
 
   const [showAnswer, setShowAnswer] = useState(false);
+
+  function handleGotIt(){
+    updateCard();
+    setShowAnswer(false);
+  }
 
   return (
     <Card fluid style={{ height: "100%" }}>
       <Card.Content style={{ backgroundColor: "lightgray" }}>
-        Question
+        {card.question}
       </Card.Content>
       {showAnswer ?
-      <>
-        <Card.Content>
-        Answer
-      </Card.Content>
-       <Button.Group compact>
-       <Button color="grey" onClick={() => console.log('didn\n know it')}>Didn't know it</Button>
-       <Button color="olive" onClick={() => console.log('got it!')}>Got it!</Button>
-     </Button.Group>
-     </>
+        <>
+          <Card.Content>
+            {card.answer}
+          </Card.Content>
+          <Button.Group compact>
+            <Button color="grey" onClick={() => console.log('didn\n know it')}>Didn't know it</Button>
+            <Button color="olive" onClick={handleGotIt}>Got it!</Button>
+          </Button.Group>
+        </>
         :
         <Button color="blue" onClick={() => setShowAnswer(true)}> Show Answer</Button>
       }
-
     </Card>
   )
 }
