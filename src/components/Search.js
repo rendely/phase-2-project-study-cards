d0020e72e183fd9f0e90c17e68600874e214e4b8
matthew rendely <matthew.rendely@gmail.com>
2023-04-27 20:52:12 -0700

    Clean up db to zero state to start adding real data

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,34 +1,34 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, Grid, Input } from 'semantic-ui-react'
 import Layout from './Layout'
 import StudyCard from './StudyCard'
 
 function Search() {
-  const [search, setSearch] = useState('constant');
+  const [search, setSearch] = useState('');
   const [cards, setCards] = useState([]);
   const numColumns = Math.floor(window.innerWidth / 300);
   useEffect(getCards, [search]);
 
   function getCards() {
-    fetch('http://localhost:3001/cards?q='+search)
+    fetch('http://localhost:3001/cards?q=' + search)
       .then(r => r.json())
       .then(cards => setCards(cards))
   }
 
-  function handleUpdateCard(updatedCard){
-     setCards(cards => cards.map(c => c.id === updatedCard.id ? updatedCard : c));
+  function handleUpdateCard(updatedCard) {
+    setCards(cards => cards.map(c => c.id === updatedCard.id ? updatedCard : c));
   }
 
   return (
     <Layout>
-      <Container style={{marginBottom: '10px'}}>
-      Search: <Input name='search' value={search} onChange={(e) => setSearch(e.target.value)} />
-      </Container>        
+      <Container style={{ marginBottom: '10px' }}>
+        Search: <Input name='search' value={search} onChange={(e) => setSearch(e.target.value)} />
+      </Container>
       <Container>
         <Grid columns={numColumns} >
           {cards.map(card =>
             <Grid.Column key={card.id}>
-              <StudyCard card={card} onUpdateCard={handleUpdateCard}/>
+              <StudyCard card={card} onUpdateCard={handleUpdateCard} />
             </Grid.Column>
           )}
         </Grid>
